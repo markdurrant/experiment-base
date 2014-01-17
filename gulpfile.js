c7a542cd3a62c9282fd65e26d6e6970317a7a5d0
Mark Durrant <mark.j.durrant.gmail.com>
2014-01-17 14:47:45 +0000

    add sass complication

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass')
 
-gulp.task('default', function(){
+// sass 
+gulp.task( 'sass', function () {
+  gulp.src( './src/sass/*.scss' )
+    .pipe( sass({
+      includePaths: [ './src/sass/includes' ],
+      outputStyle: [ 'compressed' ]
+    }))
+    .pipe( gulp.dest( './dist/css' ) );
+});
+
+gulp.task( 'default', function(){
   // place code for your default task here
 });
