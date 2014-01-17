3fcdd4e3b1e60d0d24abe73d1ec1249b91924976
Mark Durrant <mark.j.durrant.gmail.com>
2014-01-17 14:57:29 +0000

    create variables for source and distribution folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
+// Gulp.js
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var sass = require('gulp-sass')
 
-// sass 
+// Plugins
+var sass = require('gulp-sass');
+
+// source and distribution folders 
+var src = './src/';
+var dist = './dist/';
+
+// SASS task
 gulp.task( 'sass', function () {
-  gulp.src( './src/sass/*.scss' )
+  gulp.src( src + 'sass/*.scss' )
     .pipe( sass({
-      includePaths: [ './src/sass/includes' ],
+      includePaths: [ src + 'sass/includes' ],
       outputStyle: [ 'compressed' ]
     }))
-    .pipe( gulp.dest( './dist/css' ) );
+    .pipe( gulp.dest( dist + 'css' ) );
 });
 
 gulp.task( 'default', function(){
