76dfffc52ab643b59b8f2e4cfba1deb26a517314
Mark Durrant <mark.j.durrant.gmail.com>
2014-04-16 16:50:08 +0100

    start using new gulp stuff back in

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require( 'gulp' );
+
+// gulp plugins
+var sass = require( 'gulp-sass' );
+
+var paths = {
+  sass: 'src/sass/**/*.scss',
+  sassOutput: 'dist/css/'
+};
+
+// sass task
+gulp.task( 'sass', function() {
+  return gulp.src( paths.sass )
+    .pipe( sass() )
+    .pipe( gulp.dest( paths.sassOutput ) );
+});
+
+// watch task
+gulp.task( 'watch', function() {
+  gulp.watch( paths.sass, ['sass'] );
+});
+
+// default task
+gulp.task( 'default', ['watch'] );
