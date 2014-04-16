b9a429b0933934e3ded9cddd71e80b07d6b6c02f
Mark Durrant <mark.j.durrant.gmail.com>
2014-04-16 18:47:55 +0100

    fidle with beep on sass - still broke

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,44 @@ var gutil = require( 'gulp-util' );
    jshint = require( 'gulp-jshint' ),
   stylish = require( 'jshint-stylish' );
 
+// var paths = {
+//   sass: 'src/sass/**/*.scss',
+//   sassOutput: 'dist/css/',
+//   js: 'src/js/**/*.js',
+//   jsVendor: '!src/js/vendor/**/*.js',
+//   jsOutput: 'dist/js/'
+// };
+
+function beepy () {
+  gutil.beep();
+}
+
 var paths = {
-  sass: 'src/sass/**/*.scss',
-  sassOutput: 'dist/css/',
-  js: 'src/js/**/*.js',
-  jsVendor: '!src/js/vendor/**/*.js',
-  jsOutput: 'dist/js/'
+  sass: {
+    src: 'src/sass/**/*.scss',
+    dist: 'dist/css/'
+  },
+  js: {
+    src: 'src/js/**/*.js',
+    vendor: 'src/js/vendor/**/*.js',
+    dist: 'dist/js/'
+  }
 };
 
 // complie sass
 gulp.task( 'sass', function() {
-  return gulp.src( paths.sass )
+  return gulp.src( paths.sass.src )
     .pipe( sass({
       sourceComments: 'normal',
       errLogToConsole: true,
-      onError: gutil.beep()
+      onError: gutil.beep
     }))
-    .pipe( gulp.dest( paths.sassOutput ) )
+    .pipe( gulp.dest( paths.sass.dist ) )
 });
 
 // hint JS
 gulp.task( 'jshint', function() {
-  return gulp.src( [paths.js, paths.jsVendor] )
+  return gulp.src( [paths.js.src, '!' + paths.js.vendor] )
     .pipe( jshint() )
     .pipe( jshint.reporter( stylish ) )
     .pipe( jshint.reporter( gutil.beep ) );
@@ -35,8 +51,8 @@ gulp.task( 'jshint', function() {
 
 // copy JS
 gulp.task( 'copyJs', function() {
-  return gulp.src( paths.js )
-    .pipe( gulp.dest( paths.jsOutput ) );
+  return gulp.src( paths.js.src )
+    .pipe( gulp.dest( paths.js,dist ) );
 });
 
 // watch task
