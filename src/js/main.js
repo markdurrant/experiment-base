deeb7ae5f0c2507810dcf6fb454386b9b1ed1676
Mark Durrant <mark.j.durrant@gmail.com>
2018-12-14 10:33:05 +0000

    version one

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM content ready.");
+let fixPosition = el => {
+  let x = el.getBoundingClientRect().x;
+  let y = el.getBoundingClientRect().y;
+
+  el.setAttribute('style', `position: fixed; top: ${y + 'px'}; left: ${x + 'px'};`);
+}
+
+let unFixPosition = el => {
+  el.setAttribute('style', '');
+};
+
+let left = document.querySelector('.left');
+let right = document.querySelector('.right');
+
+window.addEventListener('scroll', e => {
+  let offset = window.innerHeight + 320;
+
+  if (window.scrollY > left.getBoundingClientRect().height - offset) {
+    fixPosition(left);
+  } else  {
+    unFixPosition(left);
+  }
+
+  if (window.scrollY > right.getBoundingClientRect().height - offset) {
+    fixPosition(right);
+  } else  {
+    unFixPosition(right);
+  }
 });
