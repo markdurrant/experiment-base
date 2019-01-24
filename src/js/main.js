ca6b491de8659050a3e92c4df05b38a41d856d14
Mark Durrant <mark.j.durrant@gmail.com>
2019-01-24 10:48:47 +0000

    add card component

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM content ready.");
+  document.querySelectorAll('.card-header').forEach(header => {
+    header.addEventListener('click', (e) => {
+      let cardOuter = e.srcElement.closest('.card-outer');
+
+          cardOuter.classList.toggle('open');
+          cardOuter.classList.toggle('closed');
+    });
+  });
 });
