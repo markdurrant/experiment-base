d72bc7fa219ebf61512c2cfc86417bf82daf2b96
Mark Durrant <mark.j.durrant@gmail.com>
2019-01-24 13:48:58 +0000

    Revert "add card component"
    This reverts commit ca6b491de8659050a3e92c4df05b38a41d856d14.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll('.card-header').forEach(header => {
-    header.addEventListener('click', (e) => {
-      let cardOuter = e.srcElement.closest('.card-outer');
-
-          cardOuter.classList.toggle('open');
-          cardOuter.classList.toggle('closed');
-    });
-  });
+  console.log("DOM content ready.");
 });
