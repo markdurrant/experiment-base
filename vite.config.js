6b8fb269d96ec6086a7056012e7797a2be9be978
Mark Durrant <mark.j.durrant@gmail.com>
2024-12-19 18:22:49 +0000

    Refactor vite implimentation, add vite-plugin-mkcert for https, & general cleanup

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [mkcert()], // Use mkcert plugin for easy https
+});
