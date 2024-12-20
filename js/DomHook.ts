f4dfa531ab9dbaef4e03ad4a5acbda7a94f37956
Mark Durrant <mark.j.durrant@gmail.com>
2024-12-20 09:18:42 +0000

    More DOM HOOK

--- js/DomHook.ts
+++ js/DomHook.ts
@@ -2,40 +2,89 @@ import { HTML_ELEMENT_ALL_KEYS } from "./HTMLElementKeys";
 
 type Selector = string;
 
-const _jGroup = (nodeList) => {
-  return nodeList.map((node) => _jItem(node));
+// Quality of life methods that get a lot of use
+const extraMethods = {
+  toggleClass: function (className: string) {
+    this.node.classList.toggle(className);
+  },
+  addClass: function (className: string) {
+    this.node.classList.add(className);
+  },
+  removeClass: function (className: string) {
+    this.node.classList.remove(className);
+  },
+  on: function (event: Event, fn: Function) {
+    this.node.addEventListener(event, fn);
+  },
+  off: function (event: Event, fn: Function) {
+    this.node.removeEventListener(event, fn);
+  },
+  onClick: function (fn: Function) {
+    this.on("click", fn);
+  },
+  onInput: function (fn: Function) {
+    this.on("input", fn);
+  },
 };
 
+// Group of _jitems
+const _jGroup = (nodeList) => {
+  const items = nodeList.map((node) => _jItem(node));
+
+  const _jgroup = {
+    items,
+    each: function (fn) {
+      items.map(fn);
+    },
+    getEachByProp: function (prop) {
+      return this.items.map((item) => {
+        return item[prop];
+      });
+    },
+    setEachByProp: function (prop, value) {
+      this.items.map((item) => {
+        item[prop] = value;
+      });
+    },
+  };
+
+  return _jgroup;
+};
+
+// Get the config to define the _jItem getters, setters, and methods
+// Use `propTypes` to know
 const getPropConfig = function (prop, propTypes) {
-  const get = propTypes.includes("get") && {
+  const get = {
     get() {
       return this.node[prop];
     },
   };
 
-  const set = propTypes.includes("get") && {
+  const set = {
     set(value) {
       this.node[prop] = value;
     },
   };
 
-  const method = propTypes.includes("method") && {
+  const method = {
     value: function (...args) {
       return this.node[prop](...args);
     },
   };
 
   return {
-    ...get,
-    ...set,
-    ...method,
+    ...(propTypes.includes("get") && get),
+    ...(propTypes.includes("get") && set),
+    ...(propTypes.includes("method") && method),
     enumerable: true,
   };
 };
 
+// Single DomHook item based on a node
 const _jItem = (node) => {
-  const _jitem = { node };
+  const _jitem = { node, ...extraMethods };
 
+  // For each prop in HTML KEYS create a prop config and apply or _jItem;
   for (const prop in HTML_ELEMENT_ALL_KEYS) {
     const propTypes = HTML_ELEMENT_ALL_KEYS[prop];
     const propConfig = getPropConfig(prop, propTypes);
@@ -46,6 +95,8 @@ const _jItem = (node) => {
   return _jitem;
 };
 
+// Get a nodelist from selector
+// Depending on depth create `_jItem` or `jGroup`
 export const _j = (selector: Selector) => {
   const nodeList = Array.from(document.querySelectorAll(selector));
   const listLength = nodeList.length;
